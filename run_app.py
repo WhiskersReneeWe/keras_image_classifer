55de49166816d78aa4a9039e90a3fbd956b0ada7
Renee S. Liu <reneesyliu@gmail.com>
2019-01-17 09:39:46 -0800

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -211,7 +211,7 @@ def predict():
 
     
 if __name__ == '__main__':
-
+    predict()
     # Serve the app with gevent
     http_server = WSGIServer(('127.0.0.1', 5000), app)
     http_server.serve_forever()
